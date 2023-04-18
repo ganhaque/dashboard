d4d9e42af9c04d5efc4f3c510e3104649e29b3ce
ganhaque <ganhaquegnq@gmail.com>
2023-04-17 22:45:41 -0500

    test

--- src/Components/Modules/TaskWarrior/TaskWarriorCore.tsx
+++ src/Components/Modules/TaskWarrior/TaskWarriorCore.tsx
@@ -24,6 +24,7 @@ import * as render from './Render';
 
 /* const EMPTY_JSON = "[\n]\n"; */
 
+
 function TaskWarrior() {
   const [tagNameArray, setTagNameArray] = useState<string[]>([""]);
   const [tagNameArrayInitialized, setTagNameArrayInitialized] = useState(false);
